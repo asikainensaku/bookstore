ecced8504f24222a6eda23c736d92d22fd2821fe
bhy796@myy.haaga-helia.fi <bhy796@myy.haaga-helia.fi>
2026-09-13 21:31:48 +0300

    lisätty jpa ja h2

--- src/main/java/hh/backend/bookstore/BookstoreApplication.java
+++ src/main/java/hh/backend/bookstore/BookstoreApplication.java
@@ -1,7 +1,12 @@
 package hh.backend.bookstore;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import hh.backend.bookstore.domain.Book;
+import hh.backend.bookstore.domain.BookRepository;
 
 @SpringBootApplication
 public class BookstoreApplication {
@@ -10,4 +15,21 @@ public class BookstoreApplication {
 		SpringApplication.run(BookstoreApplication.class, args);
 	}
 
+	@Bean
+	public CommandLineRunner demo(BookRepository repository) {
+		return (args) -> {
+
+			repository.save(
+					new Book("Hei me koodataan!", "Kalle Koodaaja", 2010, "1111111-12", 23.10));
+
+			repository.save(
+					new Book("Javaa javattomille", "Jaakko Javailija", 2015, "2222222-23", 19.90));
+
+			repository.save(
+					new Book("Bugit kuriin", "Pekka Ohjelmoija", 2020, "3333333-34", 25.50));
+
+			repository.save(
+					new Book("Koodarin käsikirja", "Tiina Tietokanta", 2023, "4444444-45", 29.90));
+		};
+	}
 }
