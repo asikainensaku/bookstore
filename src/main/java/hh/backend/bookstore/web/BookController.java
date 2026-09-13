46ec88f6d388286e7c5544b6b03b73e9b8d19f57
bhy796@myy.haaga-helia.fi <bhy796@myy.haaga-helia.fi>
2026-09-13 22:19:48 +0300

    tehtävä 2

--- src/main/java/hh/backend/bookstore/web/BookController.java
+++ src/main/java/hh/backend/bookstore/web/BookController.java
@@ -1,13 +1,28 @@
 package hh.backend.bookstore.web;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import hh.backend.bookstore.domain.BookRepository;
 
 @Controller
 public class BookController {
 
+    private BookRepository bookRepository;
+
+    public BookController(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
     @GetMapping("/index")
     public String showBookstore() {
         return "index"; // index.html
+    }
+
+    @GetMapping("/booklist")
+    public String bookList(Model model) {
+        model.addAttribute("books", bookRepository.findAll());
+        return "booklist";
     }
 }
